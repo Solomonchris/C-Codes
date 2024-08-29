37311d103aeae9122581f8ce359fe5614dec4abc
Solomon Christy OI <127486103+Solomonchris@users.noreply.github.com>
2024-08-29 17:39:40 +0530

    Create scanf.c

---
+++ scanf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+    float A= 1300.36000;
+    int B= 100;
+    int C;
+    char D;
+    float E;
+    /*printf("Output in Floating point: %f", A);
+    printf("\nOutput in truncated floating point: %G", A);
+    printf("\nOutput in Exponential Floating point: %E", A);
+    printf("\nOutput in Integer: %-5d %d", B, B);*/
+    printf("\nEnter value of C: ");
+    scanf("%d", &C);
+    printf("\nEnter name of a person: ");
+    scanf("%s", &D);
+    printf("\nEnter a decimal number: ");
+    scanf("%f", &E);
+}
