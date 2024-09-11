14d09ecd26cf5f72a42a223830ad24bfc13b29d3
Solomon Christy OI <127486103+Solomonchris@users.noreply.github.com>
2024-09-11 08:30:12 +0530

    Update Factorial_using_recursion.c

--- Factorial_using_recursion.c
+++ Factorial_using_recursion.c
@@ -4,13 +4,22 @@ int main() {
     int n;
     printf("Enter a positive integer: ");
     scanf("%d",&n);
-    printf("Factorial of %d = %ld", n, fact(n));
+    if (n<0)
+    {
+        printf("Invalid Enter a positive number");
+    }
+    else
+    {
+        printf("\nFactorial of %d = %ld", n, fact(n));
+    }
     return 0;
 }
 
-long int fact(int n) {
+long int fact(int n) 
+{
     if (n>=1)
         return n*fact(n-1);
+        
     else
         return 1;
 }
